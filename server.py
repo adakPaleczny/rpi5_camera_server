5f79b910605e49cdc8067286d3425a49c06eba38
adakPaleczny <adampaleczny02@gmail.com>
2024-02-23 13:25:48 +0000

    working with authorization

--- server.py
+++ server.py
@@ -1,39 +1,33 @@
-from flask import Flask, Response
+from flask import Flask, Response, render_template, request, redirect, session
 import cv2
-
+import sqlite3
 from camera_recognition.camera_vision import VisionCamera  # Ensure this import works correctly
 
 app = Flask(__name__)
 pi_camera = VisionCamera()  # Ensure the camera is initialized correctly here
 
 USE_MODEL = False
+app.secret_key = '692137_papierz'  # Set to a random string
 
-@app.route('/')
+
+@app.route('/', methods=["GET","POST"])
 def index():
     # HTML template for the index page with a link to the live video page
-    html = """
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>Camera Feed</title>
-    </head>
-    <body>
-        <input type="text" id="username" name="username"><br><br>
-        <input type="password" id="password" name="password"><br><br>
-        <input type="submit" value="Login">
-        <h1>Welcome to the Live Camera Feed</h1>
-        <p>Click <a href="/video">here</a> to view the live video stream.</p>
-    </body>
-
-    <script>
-        function log(){
-            l = document.getElementById('username');
-            p = document.getElementById('password');
-        }
-    </script>
-    </html>
-    """
-    return html
+    if request.method == "POST":
+        user = request.form.get('username')
+        password = request.form.get('password')
+        
+        # return f'{user}, your username is {password}'
+        db = sqlite3.connect("database/data.db")
+        cur = db.cursor()
+        res = cur.execute("SELECT user_id FROM osoby WHERE login = ? AND pass = ?", (user, password)).fetchone()
+        if(res):
+            session['is_authorized'] = True
+            return redirect('/video')
+        else:
+            return f'No authorization'
+    session['is_authorized'] = False
+    return render_template('index.html')
 
 def gen(pi_cam):
     """Video streaming generator function."""
@@ -53,6 +47,9 @@ def gen(pi_cam):
 @app.route('/video')
 def video():
     """Video streaming route. Put this in the src attribute of an img tag."""
+    if not session.get('is_authorized', False):
+        return redirect("/")
+
     return Response(gen(pi_camera),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
